33a3f6dc12690334caabf8f53f23772b9170b467
haleanana <haleanana@gmail.com>
2020-12-02 14:19:59 +0000

    Added checkout views and templates

--- online_store/settings.py
+++ online_store/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
 
+    #other
+    'crispy_forms',
+
+
 ]
 
 MIDDLEWARE = [
@@ -59,6 +63,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'online_store.urls'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -74,8 +79,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
