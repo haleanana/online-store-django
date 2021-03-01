4306709b650d708cf43ad8dbf74e2d59bb3f4724
haleanana <haleanana@gmail.com>
2021-03-01 19:48:42 +0000

    Product Admin - Deleting products

--- products/views.py
+++ products/views.py
@@ -75,9 +75,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Suceessfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid')
     else:
@@ -114,4 +114,9 @@ def edit_product(request, product_id):
 
     return render(request, template, context)
 
-
+def delete_product(request, product_id):
+    """Delete a product""" 
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product successfully deleted')
+    return redirect(reverse('products'))
