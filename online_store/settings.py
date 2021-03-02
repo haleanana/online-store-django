d10e604a22c951ee2d9a75a22767f1e3539ec640
haleanana <haleanana@gmail.com>
2021-03-02 18:08:56 +0000

    Removed secret key and set debug

--- online_store/settings.py
+++ online_store/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7k2s+@+s@7moh!wz*-zpei0ce(nmj)j8i4t$)@tjuq$7zx85^+'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['online-store-boutique-ado.herokuapp.com', 'localhost']
 
